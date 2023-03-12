ab4658ee82a250f42de9a4cd53bf9c15ef7836f6
Danielsan-80 <countzero80@gmail.com>
2023-03-12 15:36:53 +0100

    test-21

--- models/User.js
+++ models/User.js
@@ -30,53 +30,53 @@ const userSchema = new Schema({
 
 }, {timestamps: true})
 
-userSchema.statics.register = async function(name, email, password) {
+// userSchema.statics.register = async function(name, email, password) {
 
-    if(!name || !email || !password) {
-        throw Error('compilare tutti i campi del modulo')
-    }
+//     if(!name || !email || !password) {
+//         throw Error('compilare tutti i campi del modulo')
+//     }
 
-    if(!validator.isEmail(email)){
-        throw Error('inserire un indirizzo email valido')
-    }
+//     if(!validator.isEmail(email)){
+//         throw Error('inserire un indirizzo email valido')
+//     }
 
-    if(!validator.isStrongPassword(password)){
-        throw Error('le password devono contenere un misto di caratteri alfanumerici e simboli')
-    }
+//     if(!validator.isStrongPassword(password)){
+//         throw Error('le password devono contenere un misto di caratteri alfanumerici e simboli')
+//     }
 
-    const exists = await this.findOne({email})
+//     const exists = await this.findOne({email})
 
-    if(exists) {
-        throw Error('questo utente è già presente nel nostro database')
-    }
+//     if(exists) {
+//         throw Error('questo utente è già presente nel nostro database')
+//     }
 
-    const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash(password, salt)
+//     const salt = await bcrypt.genSalt(10)
+//     const hash = await bcrypt.hash(password, salt)
 
-    const user = await this.create({name, email, password: hash})
+//     const user = await this.create({name, email, password: hash})
 
-    return user
-}
+//     return user
+// }
 
-userSchema.statics.login = async function(email, password) {
+// userSchema.statics.login = async function(email, password) {
 
-    if(!email || !password) {
-        throw Error('compilare tutti i campi del modulo')
-    }
+//     if(!email || !password) {
+//         throw Error('compilare tutti i campi del modulo')
+//     }
 
-    const user = await this.findOne({email})
+//     const user = await this.findOne({email})
 
-    if(!user) {
-        throw Error('questo utente non è presente nel nostro database')
-    }
+//     if(!user) {
+//         throw Error('questo utente non è presente nel nostro database')
+//     }
 
-    const match = await bcrypt.compare(password, user.password)
+//     const match = await bcrypt.compare(password, user.password)
 
-    if(!match) {
-        throw Error('password non corretta')
-    }
+//     if(!match) {
+//         throw Error('password non corretta')
+//     }
 
-    return user
-}
+//     return user
+// }
 
 module.exports = mongoose.model('User', userSchema)
