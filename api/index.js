f833906ecec4dbce1968b1a746647236d9b0345a
Danielsan-80 <countzero80@gmail.com>
2023-03-12 14:25:29 +0100

    test-8

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 const postRouter = require('../routes/posts')
 const app = require('express')();
+const mongoose = require('mongoose')
 
 app.use('/api/posts', postRouter)
+
+mongoose.connect().then(()=>{
+    console.log('connected to mongodb')
+})
 
 module.exports = app;
--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,7 @@
 const express = require('express')
 const router = express.Router()
 require ('dotenv').config()
-const mongoose = require('mongoose')
 const Post = require('../models/Post')
-
-mongoose.connect(process.env.MONGODB_URI)
-.then(()=>{
-
-  console.log('connected to mongodb')
 
   router.get('/', async (req, res)=>{
     const posts = await Post.find({}).populate('author').sort({updatedAt: -1})
@@ -20,8 +14,6 @@ router.get('/:slug', (req, res) => {
   const { slug } = req.params;
   res.status(200).json({"post": slug});
 });
-
-})
 
 
 module.exports = router
