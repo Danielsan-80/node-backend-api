2ec40aeb9a7869819af838704e27602c6fa4bd74
Danielsan-80 <countzero80@gmail.com>
2023-03-12 13:52:48 +0100

    test-3

--- api/index.js
+++ api/index.js
@@ -1,4 +1,7 @@
+const postRouter = require('../routes/posts')
 const app = require('express')();
+
+app.use('api/posts', postRouter)
 
 app.get('/', (req, res)=>{
     res.status(200).json({"message":"home"})
