62f1f927d62c4760e6ffba3330be29c3ce906156
Danielsan-80 <countzero80@gmail.com>
2023-03-12 15:16:06 +0100

    test-18

--- routes/posts.js
+++ routes/posts.js
@@ -19,7 +19,7 @@ router.get('/', async (req, res)=>{
 })
 
 router.get('/:id', async(req, res) => {
-  const { _id } = req.params;
+  const _id  = req.params.id;
   const post = await Post.findById(_id)
   res.status(200).json(post);
 });
