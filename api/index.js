1e0903c7da245f65e13ef48304dec33cc6866060
Danielsan-80 <countzero80@gmail.com>
2023-03-12 14:39:14 +0100

    test-10

--- api/index.js
+++ api/index.js
@@ -5,8 +5,12 @@ require ('dotenv').config()
 
 app.use('/api/posts', postRouter)
 
-mongoose.connect(process.env.MONGODB_URI).then(()=>{
-    console.log('connected to mongodb')
-})
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 
 module.exports = app;
