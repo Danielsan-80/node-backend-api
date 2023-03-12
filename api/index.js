98bfecfab660c63a484c50c4346f5ad6f26b1740
Danielsan-80 <countzero80@gmail.com>
2023-03-12 13:54:41 +0100

    test-4

--- api/index.js
+++ api/index.js
@@ -3,24 +3,24 @@ const app = require('express')();
 
 app.use('api/posts', postRouter)
 
-app.get('/', (req, res)=>{
-    res.status(200).json({"message":"home"})
-})
+// app.get('/', (req, res)=>{
+//     res.status(200).json({"message":"home"})
+// })
 
-app.get('/api', (req, res) => {
+// app.get('/api', (req, res) => {
 
-  res.status(200).json({"message":"backend node api test"})
+//   res.status(200).json({"message":"backend node api test"})
 
-});
+// });
 
 
-app.get('/api/:slug', (req, res) => {
+// app.get('/api/:slug', (req, res) => {
 
-  const { slug } = req.params;
+//   const { slug } = req.params;
 
-  res.status(200).json({"Item": slug});
+//   res.status(200).json({"Item": slug});
 
-});
+// });
 
 
 module.exports = app;
