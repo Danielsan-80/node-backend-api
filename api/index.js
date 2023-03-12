ebd95d1cfe1fd8e0c0ca8a1f2f9f397be67c0e9e
Danielsan-80 <countzero80@gmail.com>
2023-03-12 14:44:06 +0100

    test-11

--- api/index.js
+++ api/index.js
@@ -1,16 +1,9 @@
 const postRouter = require('../routes/posts')
 const app = require('express')();
-const mongoose = require('mongoose')
-require ('dotenv').config()
+
 
 app.use('/api/posts', postRouter)
 
-mongoose.connect(process.env.MONGODB_URI)
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
-});
 
 module.exports = app;
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const Post = require('../models/Post')
+const mongoose = require('mongoose')
+require ('dotenv').config()
 
   router.get('/', async (req, res)=>{
+    mongoose.connect(process.env.MONGODB_URI)
+
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "connection error: "));
+    db.once("open", function () {
+    console.log("Connected successfully")
+
+    
+});
+
     const posts = await Post.find({}).populate('author').sort({updatedAt: -1})
 
     res.status(200).json(posts)
