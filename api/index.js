3082ffa1d9712c14cdf1a5ae29a37fc1060def8b
Danielsan-80 <countzero80@gmail.com>
2023-03-16 15:07:19 +0100

    api-cors

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const postRouter = require('../routes/posts')
 const userRouter = require('../routes/users')
 const express = require('express');
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use('/api/posts', postRouter)
 app.use('/api/users', userRouter)
 
