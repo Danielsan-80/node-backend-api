34100914954bdbd4c8bd290d312bf7e217c6f8d8
Danielsan-80 <countzero80@gmail.com>
2023-07-14 19:36:05 +0200

    node-backend-api-img 35

--- routes/posts.js
+++ routes/posts.js
@@ -35,9 +35,9 @@ router.get('/:id', async(req, res) => {
 });
 
 router.post('/', async(req, res)=>{
-  const {email, title, body, category, tags} = req.body
+  const {email, title, body, featuredImg, category,  tags} = req.body
   
-  return req.files
+  return JSON.stringify(req.body)
     try {
       
       let img = req.files?.featuredImg || {}
